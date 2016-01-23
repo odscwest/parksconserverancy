c4f0038d1c0f7ca6595ef2fb90f7d913196d30da
laurieskelly <laurie@datasco.pe>
2016-01-23 11:12:29 -0800

    fixed keys

--- define_column.py
+++ define_column.py
@@ -1,23 +1,29 @@
 #coding=UTF-8
 '''
-Contains one function `define_column(col_name)` that prints and returns the definition of a column from the golden gate conservancy dataset.
+Contains one function `define_column(col_name)` that prints the definition of a column from the golden gate conservancy dataset.
 '''
 
 
 DEFINITIONS = {
-    'Site-Year Code':
+    'Site-YearCode':
     'Four letter shortening of the full site name using the first two letters of the first word and first two letters of the second word. \n\nIf the site has more than two words in the name, initials  are used (e.g. Sutro Dunes = SUDO, Navy Memorial Slope = NMS). \n\nThe year the observations were made is attached to the site name (e.g. all observations taken in 2012 will have xxxx-2012 as a name).',
 
-    'Transect Number':
+    'Transect':
     'The discrete line along which observations are made. \n\nIn this study this could be any number between zero and infinity, but should be sequential and at regular intervals.',
 
-    'Point Number':
+    'Point':
     'Discrete locations along the transect at predetermined intervals where observations are made. \n\nIn this case a dowel rod is dropped perpendicular to the tape and parallel to a standing person to the ground. In this study this could be any number between zero and infinity, though the numbers should be sequential and at regular intervals.',
 
     'Height':
     'Distance from the ground where plants cross the point on the transect. In this study, height classes were used:\n- Low = 0 to 0.5 meters\n- Medium = 0.51 to 2.0 meters\n- High = 2.1 to 15 meters\n- S (for super high) = 15+ meters',
 
+    'Height ':
+    'Distance from the ground where plants cross the point on the transect. In this study, height classes were used:\n- Low = 0 to 0.5 meters\n- Medium = 0.51 to 2.0 meters\n- High = 2.1 to 15 meters\n- S (for super high) = 15+ meters',
+
     'Scientific Name':
+    'The Latin genus and species assigned to the plant based on the Jepson Manual of California (1993 version, it has since been updated with new names in 2012). \n\nThe Jepson can be accessed online at ucjeps.berkeley.edu or on CalFlora at www.calflora.org',
+
+    'Species':
     'The Latin genus and species assigned to the plant based on the Jepson Manual of California (1993 version, it has since been updated with new names in 2012). \n\nThe Jepson can be accessed online at ucjeps.berkeley.edu or on CalFlora at www.calflora.org',
 
     'Plant Code':
@@ -38,4 +44,4 @@ def define_column(col_name):
     '''
     coldef = DEFINITIONS[col_name]
     print coldef
-    return coldef
+    return
